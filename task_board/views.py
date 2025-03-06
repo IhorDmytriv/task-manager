35b52291105abfbc9dc8fa2fe3f0f6651ce9cd1a
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-03-06 20:22:05 +0100

    Create index view for homepage in views.py

--- task_board/views.py
+++ task_board/views.py
@@ -1,3 +1,12 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+from task_board.models import Task
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    task_list = Task.objects.all()
+    context = {
+        "task_list": task_list
+    }
+    return render(request, "task_board/index.html", context=context)
