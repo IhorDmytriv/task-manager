3f5617f26c9cc849c920a0347763a5450053f467
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-03-16 10:51:37 +0100

    Update requirements.txt & configure Django Debug Tool

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("task_board/", include("task_board.urls", namespace="task_board")),
-]
+] + debug_toolbar_urls()
