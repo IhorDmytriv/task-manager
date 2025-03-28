b8c279c882f1d5fbd17315ec2c18e2117ac6c447
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-03-28 19:32:30 +0100

    Remove unnecessary spaces and adjust line lengths

--- task_board/urls.py
+++ task_board/urls.py
@@ -10,7 +10,11 @@ urlpatterns = [
     path("", index, name="index"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
     path("workers/", WorkerListView.as_view(), name="worker-list"),
-    path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
+    path(
+        "workers/<int:pk>/",
+        WorkerDetailView.as_view(),
+        name="worker-detail"
+    ),
 ]
 
 app_name = "task_board"
