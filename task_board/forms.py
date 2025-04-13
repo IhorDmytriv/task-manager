4f94b430aed731a9043b2a1fb8ab90bdb5ae3bb3
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-04-13 20:32:10 +0200

    Add validation for past dates in Task deadline

--- task_board/forms.py
+++ task_board/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 from task_board.models import Task, Worker
 
@@ -30,6 +32,12 @@ class TaskForm(forms.ModelForm):
                 attrs={"type": "date", "class": "form-control"}
             ),
         }
+
+    def clean_deadline(self):
+        deadline = self.cleaned_data["deadline"]
+        if deadline < timezone.now().date():
+            raise ValidationError("Deadline cannot be in the past.")
+        return deadline
 
 
 class WorkerCreateForm(forms.ModelForm):
